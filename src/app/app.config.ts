642f1a6fbdbe2650a55b682a15af2d126b7a7986
futuremake <78623051+futuremake@users.noreply.github.com>
2026-06-11 14:35:18 -0600

    Commented out "importProvidersFrom"
    Updated app.config.ts to comment out "importProvidersFrom". Research found that it is no longer needed in builds.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,14 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
+    // importProvidersFrom(
+    //   provideFirebaseApp(() => initializeApp(environment.firebase)),
+    //   provideFirestore(() => getFirestore()),
+    //   provideAuth(() => getAuth()),
+    //   provideFunctions(() => getFunctions()),
+    //   provideStorage(() => getStorage()),
+    //   provideMessaging(() => getMessaging())
+    // ),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"fir-demo-5a5e9","appId":"1:884166110716:web:5c8bda928ac375e12f9679","storageBucket":"fir-demo-5a5e9.firebasestorage.app","apiKey":"AIzaSyAC8ZV66C0biakn-xsB6x2zBua3Somb0qw","authDomain":"fir-demo-5a5e9.firebaseapp.com","messagingSenderId":"884166110716","projectNumber":"884166110716","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
