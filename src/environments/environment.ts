0bcff821b964436d1e7521693e8156dca1287955
Ernest Niiquaye <eniiqua@wgu.edu>
2026-06-11 22:25:48 -0600

    Updated 'environment.ts' to import and configure Firebase in the app. Updated 'chat.service.ts' to enable login and logout functionality in the app.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,21 @@
 export const environment = {
+  // firebase: {
+  //   apiKey: 'API_KEY',
+  //   authDomain: 'PROJECT_ID.firebaseapp.com',
+  //   databaseURL: 'https://PROJECT_ID.firebaseio.com',
+  //   projectId: 'PROJECT_ID',
+  //   storageBucket: 'PROJECT_ID.appspot.com',
+  //   messagingSenderId: 'SENDER_ID',
+  //   appId: 'APP_ID',
+  //   measurementId: 'G-MEASUREMENT_ID',
+  // },
+
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
-  },
+    apiKey: 'AIzaSyAC8ZV66C0biakn-xsB6x2zBua3Somb0qw',
+    authDomain: 'fir-demo-5a5e9.firebaseapp.com',
+    projectId: 'fir-demo-5a5e9',
+    storageBucket: 'fir-demo-5a5e9.firebasestorage.app',
+    messagingSenderId: '884166110716',
+    appId: '1:884166110716:web:5c8bda928ac375e12f9679',
+  }
 };
